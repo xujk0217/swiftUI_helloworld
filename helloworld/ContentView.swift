8168a28f80c1bea512d870c93c5f648314a60117
xujk <kevin16021777@gmail.com>
2024-01-15 18:11:39 +0800

    Initial Commit

---
+++ helloworld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by 許君愷 on 2024/1/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by 許君愷 on 2024/1/15.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
