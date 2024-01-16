1af66610f38b84167816fcd5bd4469f347b40ec9
xujk <kevin16021777@gmail.com>
2024-01-16 20:49:44 +0800

    feat : vstask, text, buttom

--- helloworld/ContentView.swift
+++ helloworld/ContentView.swift
@@ -6,14 +6,51 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    //let synthesizer = AVSpeechSynthesizer()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Guess the movie").fontWeight(.bold)
+                .font(.title)
+            Text("Can you guess the movie from these emoji?")
+            Button{
+                
+            }label: {
+                Text("😁😇")
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.green)
+            .cornerRadius(10)
+            Button{
+                
+            }label: {
+                Text("🙃")
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(10)
+            Button{
+                
+            }label: {
+                Text("😁😇🫢🫡")
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.purple)
+            .cornerRadius(10)
+            Button{
+                
+            }label: {
+                Text("😶‍🌫️😁🫣😇🫥")
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.pink)
+            .cornerRadius(10)
         }
         .padding()
     }
