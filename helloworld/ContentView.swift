8a80ff163ce1e8d259ea62d89da3f30de8e84e57
xujk <kevin16021777@gmail.com>
2024-01-16 21:30:44 +0800

    feat : text(hello world)

--- helloworld/ContentView.swift
+++ helloworld/ContentView.swift
@@ -12,6 +12,15 @@ struct ContentView: View {
     //let synthesizer = AVSpeechSynthesizer()
     var body: some View {
         VStack {
+            Text("hello world !!").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .fontDesign(.rounded)
+                .font(.title3)
+                .padding()
+                .background(Color.clear)
+                .cornerRadius(10)
+        }.padding()
+        Spacer()
+        VStack {
             Text("Guess the movie").fontWeight(.bold)
                 .font(.title)
             Text("Can you guess the movie from these emoji?")
@@ -24,6 +33,7 @@ struct ContentView: View {
             .foregroundColor(.white)
             .background(Color.green)
             .cornerRadius(10)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             Button{
                 
             }label: {
@@ -33,6 +43,7 @@ struct ContentView: View {
             .foregroundColor(.white)
             .background(Color.blue)
             .cornerRadius(10)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             Button{
                 
             }label: {
@@ -42,6 +53,7 @@ struct ContentView: View {
             .foregroundColor(.white)
             .background(Color.purple)
             .cornerRadius(10)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             Button{
                 
             }label: {
@@ -51,8 +63,11 @@ struct ContentView: View {
             .foregroundColor(.white)
             .background(Color.pink)
             .cornerRadius(10)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
         }
         .padding()
+        Spacer()
+        Spacer()
     }
 }
 
