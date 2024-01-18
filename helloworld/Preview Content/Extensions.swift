cadae09ba2ea41330a185374263d60fa271b478b
xujk <kevin16021777@gmail.com>
2024-01-19 00:25:53 +0800

    feat : refactor foodpicker, add Extensions file

---
+++ helloworld/Preview Content/Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Extensions.swift
+//  helloworld
+//
+//  Created by 許君愷 on 2024/1/19.
+//
+
+import SwiftUI
+
+extension View{
+    func mainButtonStyle() -> some View{
+        buttonBorderShape(.capsule)
+        .controlSize(.large)
+    }
+}
+
+extension Animation{
+    static let mySpring = Animation.spring(dampingFraction: 0.55)
+    static let myEase = Animation.easeInOut(duration: 0.6)
+}
+
+extension Color{
+    static let bg2 = Color(.secondarySystemBackground)
+}
